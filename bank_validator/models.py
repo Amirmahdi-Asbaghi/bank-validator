9990b777749751fd9bc73dc121a1a7dbf8da3777
amirmahdi <amirmahdiasbaghi8181@gmail.com>
2026-09-18 13:49:44 +0330

    some change for db and admin page

--- bank_validator/models.py
+++ bank_validator/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # the only table in the db
 class BankRecord(models.Model):
     # defines the allowed input modes for records
-    # i use nested class for better scope 
+    # I use nested class for better scope
     # source type only meaning 
     # "BankRecord.SourceType.JSON_FILE" better understanding access for later use
 
@@ -22,7 +22,7 @@ class BankRecord(models.Model):
     # the columns I add to the data
     run_id = models.UUIDField(default=uuid.uuid4, db_index=True) # uuid -> Universally Unique Identifiers (128-bit)
     timestamp = models.DateTimeField(auto_now_add=True)
-    source_type = models.CharField(max_length=20, choices=SourceType.choices)
+    source_type = models.CharField(max_length=20, choices=SourceType)
     file_name = models.CharField(max_length=255, blank=True, default="")
 
     # the columns from data
@@ -35,12 +35,12 @@ class BankRecord(models.Model):
     credit = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
     balance = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
 
-    valid = models.BooleanField(default=False)
-    errors = models.JSONField(default=list, blank=True)
+    valid = models.BooleanField(default=False) # sqlite stores it as 0 , 1
+    errors = models.CharField(max_length=70, blank=True, default="") # max 16 errors E00...E015 and some extra
 
     class Meta:
-        ordering = ["-timestamp"]
-        indexes = [models.Index(fields=["run_id", "valid"])]
+        ordering = ["-timestamp"] # default order of query results
+        indexes = [models.Index(fields=["run_id", "valid"])] # create composite index on two columns
 
     def __str__(self):
         return f"{self.run_id} | {self.bank_code} | {self.account_code}"
