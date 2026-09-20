59593d100c9afc82ede9974ebe4359901c085dd3
amirmahdi <amirmahdiasbaghi8181@gmail.com>
2026-09-20 03:59:26 +0330

    change admin.py

--- bank_validator/admin.py
+++ bank_validator/admin.py
@@ -9,7 +9,7 @@ class BankRecordAdmin(admin.ModelAdmin):
                     "bank_code", "period", "account_code", "debit", "credit", "balance",
                     "valid", "errors")
 
-    list_filter = ("valid", "errors")
+    list_filter = ("valid")
     search_fields = ("run_id", "timestamp",
                     "bank_code", "period", "account_code", "debit", "credit", "balance",
                     "valid", "errors")
